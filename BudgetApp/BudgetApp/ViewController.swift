f0aefd7d98c6c08e389f9c7b36bd57eebc0b4283
Tanner Jackley <tannerjackley@gmail.com>
2026-03-17 11:55:15 -0500

    Created Project in XCode

---
+++ BudgetApp/BudgetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetApp
+//
+//  Created by Jackley,Tanner D on 3/17/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BudgetApp/BudgetAppTests/BudgetAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppTests.swift
+//  BudgetAppTests
+//
+//  Created by Jackley,Tanner D on 3/17/26.
+//
+
+import Testing
+@testable import BudgetApp
+
+struct BudgetAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
