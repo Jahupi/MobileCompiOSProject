e8e7777ad238215f902f06c3319e55569b4ac6c5
Tanner Jackley <tannerjackley@gmail.com>
2026-03-28 22:20:23 -0500

    basic login/sign up screen working

--- BudgetApp/BudgetApp/ViewController.swift
+++ BudgetApp/BudgetApp/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailOutlet: UITextField!
+    @IBOutlet weak var passwordOutlet: UITextField!
+    @IBOutlet weak var statusOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signUpAction(_ sender: Any) {
+        Auth.auth().createUser(withEmail: emailOutlet.text!, password: passwordOutlet.text!) { (result, error) in
+            if let error = error {
+                self.statusOutlet.text = "Error signing in: \(error.localizedDescription)"
+            } else {
+                self.statusOutlet.text = "Signed up with email: \(self.emailOutlet.text!)"
+            }
+        }
+    }
+    
+    @IBAction func loginAction(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailOutlet.text!, password: passwordOutlet.text!) { (result, error) in
+            if let error = error {
+                self.statusOutlet.text = "Error signing in: \(error.localizedDescription)"
+            } else {
+                self.statusOutlet.text = "Signed in with email: \(self.emailOutlet.text!)"
+                self.performSegue(withIdentifier: "loginSegue", sender: self)
+            }
+        }
+    }
+    
 }
 
